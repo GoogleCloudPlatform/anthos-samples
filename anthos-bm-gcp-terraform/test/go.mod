7bc0fa04880148def5dee12ae9a9fda528c09f24
shabirmean <7249208+Shabirmean@users.noreply.github.com>
2021-04-22 00:47:21 -0400

    (chore): Add initial unit test

---
+++ anthos-bm-gcp-terraform/test/go.mod
@@ -0,0 +1,8 @@
+module github.com/GoogleCloudPlatform/anthos-samples
+
+go 1.16
+
+require (
+	github.com/gruntwork-io/terratest v0.32.24
+	github.com/stretchr/testify v1.7.0
+)
---
+++ anthos-bm-gcp-terraform/test/module_external_ip_test.go
@@ -0,0 +1,59 @@
+package test
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/gcp"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatedExternalIps(t *testing.T) {
+	t.Parallel()
+
+	exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../", "modules/external-ip")
+	projectId := gcp.GetGoogleProjectIDFromEnvVar(t) // from GOOGLE_CLOUD_PROJECT
+	region := gcp.GetRandomRegion(t, projectId, nil, nil)
+
+	randomVmHostNameOne := gcp.RandomValidGcpName()
+	randomVmHostNameTwo := gcp.RandomValidGcpName()
+	randomVmHostNameThree := gcp.RandomValidGcpName()
+	expectedIpNames := []string{
+		randomVmHostNameOne, randomVmHostNameTwo, randomVmHostNameThree}
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: exampleDir,
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"region":   region,
+			"ip_names": expectedIpNames,
+		},
+	})
+	defer terraform.Destroy(t, terraformOptions)
+
+	// run `terraform init` and `terraform apply`
+	terraform.InitAndApply(t, terraformOptions)
+	// Run `terraform output` to get the value of 'ips' output
+	ipAddressDetails := terraform.OutputMapOfObjects(t, terraformOptions, "ips")
+
+	// validate that the output contians an entry per name in expectedIpNames
+	for _, ip := range expectedIpNames {
+		errMsg := fmt.Sprintf("Output from external-ip module should have IP name: %s", ip)
+		assert.Contains(t, ipAddressDetails, ip, errMsg)
+	}
+	// validate that each ip object consistes of a `tier`, `address` and `region` attributes
+	for _, detail := range ipAddressDetails {
+		detailsMap := detail.(map[string]interface{})
+		errMsg := fmt.Sprintf("external-ip module output object's region does not match selected region: %s", region)
+		assert.Contains(t, detailsMap, "tier", "external-ip module output object does not have attribute: tier")
+		assert.Contains(t, detailsMap, "address", "external-ip module output object does not have attribute: address")
+		assert.Contains(t, detailsMap, "region", "external-ip module output object does not have attribute: region")
+
+		ipAddress := fmt.Sprintf("%v", detailsMap["address"])
+		assert.Equal(t, detailsMap["region"], region, errMsg)
+		assert.True(t, net.ParseIP(ipAddress) != nil)
+	}
+}
