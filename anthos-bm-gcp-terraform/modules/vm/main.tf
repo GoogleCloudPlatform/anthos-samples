a2966cb27240ce423a9c2e5686c2c4e5f3586325
Shabir Mohamed Abdul Samadh <7249208+Shabirmean@users.noreply.github.com>
2021-04-27 14:14:57 -0400

    AddLicense headers to all the files (#25)
    * chore: add license text to files
    
    * style: add license headers to previous samples
    
    * improvement: add ci workflows using GH actions
    
    * chore(ci): add github actions workflow
    
    * reafactor: remove set -x from the gh actions flow
    
    * refactor: add continue on error to test actions step
    
    * fix: move continue_on_error to correct place
    
    * fix: move continue_on_error to correct place
    
    * fix: move continue_on_error to inside checkstyle step
    
    * refactor: add PR comments for failutes
    
    * fix: add pr comment for CI tasks
    
    fix: add EOF to close HEREDOC
    
    Fix: set -x for heredoc usage
    
    fix: add forward slash to escape backtics
    
    fix: switch forward slash with backward slash
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    parent 9789040557ccb8cd098265575f3f59868a9f8479
    author shabirmean <7249208+Shabirmean@users.noreply.github.com> 1619502315 -0400
    committer shabirmean <7249208+Shabirmean@users.noreply.github.com> 1619504310 -0400
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: debug commit
    
    * fix: add license to missing files
    
    * fix: reformat terraform files
    
    * fix: add check for golint errors
    
    * fix: add check for golint errors

--- anthos-bm-gcp-terraform/modules/vm/main.tf
+++ anthos-bm-gcp-terraform/modules/vm/main.tf
@@ -1,9 +1,25 @@
-module external_ip_addresses {
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "external_ip_addresses" {
   source   = "../external-ip"
   ip_names = var.vm_names
 }
 
-module compute_instance {
+module "compute_instance" {
   source            = "terraform-google-modules/vm/google//modules/compute_instance"
   instance_template = var.instance_template
   region            = var.region
