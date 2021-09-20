fa61a2b98a83ef6ed92b1ab012a5d8a3caa63c89
bharathkkb <bharathkrishnakb@gmail.com>
2021-09-20 13:49:36 -0500

    lint

--- test/integration/abm_gce_defaults_on_editor_project/abm_gce_editor_test.go
+++ test/integration/abm_gce_defaults_on_editor_project/abm_gce_editor_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package abm_gce_defaults_on_editor_project
+package abmeditor
 
 import (
 	"fmt"
