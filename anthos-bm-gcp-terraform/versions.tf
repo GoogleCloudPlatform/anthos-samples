1347a10e400bdc80602b77c019b61564e6f8415c
shabirmean <shabirmean@google.com>
2022-01-17 18:59:56 -0500

    fix: update the ABM version on the resources file

--- anthos-bm-gcp-terraform/versions.tf
+++ anthos-bm-gcp-terraform/versions.tf
@@ -16,14 +16,9 @@
 
 terraform {
   provider_meta "google" {
-    # the following versions are only bumped on releases; thus the module
-    # versions below might be different from what's used in the terraform
-    # scripts on the master branch
+    # Anthos Bare metal version used in this release is 1.9
+    # See https://github.com/GoogleCloudPlatform/anthos-samples/blob/main/anthos-bm-gcp-terraform/resources/anthos_gce_cluster.tpl#L20
     module_name           = "anthos-samples/terraform/anthos-bm-terraform:gce/v0.1.0"
-    gcloud                = "terraform-google-modules/gcloud/google/v2.1.0"
-    gcp_apis              = "terraform-google-modules/project-factory/google//modules/project_services/v10.3.2"
-    gcp_service_accounts  = "terraform-google-modules/service-accounts/google/v~> 4.0"
-    gcp_compute_instances = "terraform-google-modules/vm/google//modules/compute_instance/v~> 6.3.0"
   }
   required_version = "< 1.2"
   required_providers {
