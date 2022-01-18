82f148c49bdec99762bc8a27122c245459b0e0af
shabirmean <shabirmean@google.com>
2022-01-17 19:21:34 -0500

    fix: linting error

--- anthos-bm-gcp-terraform/versions.tf
+++ anthos-bm-gcp-terraform/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   provider_meta "google" {
     # Anthos Bare metal version used in this release is 1.10
-    # See 
+    # See
     # - https://github.com/GoogleCloudPlatform/anthos-samples/blob/main/anthos-bm-gcp-terraform/resources/anthos_gce_cluster.tpl#L20
     # - https://github.com/GoogleCloudPlatform/anthos-samples/blob/main/anthos-bm-gcp-terraform/resources/init.sh#L180
     module_name = "anthos-samples/terraform/anthos-bm-terraform:gce/v0.1.0"
