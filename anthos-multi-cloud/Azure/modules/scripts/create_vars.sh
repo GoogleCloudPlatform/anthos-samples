6cf8651fd05b196a32822e1facdd8582ebb9935d
Christine Kim <35876108+ckim328@users.noreply.github.com>
2022-03-02 13:14:57 -0500

    cleanup: adding DRIFT region tags to yaml and sh files (#173)
    * Added tags for .yaml
    
    * Added tags for .yml
    
    * Added tags to .sh files
    
    * Fixed a tag
    
    * Add whitespace for tests
    
    * Removed semicolons in region tag
    
    * Added back whitespaces in test file
    
    * Correcting tf anthos_bm tags

--- anthos-multi-cloud/Azure/modules/scripts/create_vars.sh
+++ anthos-multi-cloud/Azure/modules/scripts/create_vars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START anthos_scripts_create_vars]
 
 # create vars.sh file
 set -e
@@ -27,3 +29,5 @@ export SSH_PUBLIC_KEY=${5}
 export SUBNET_ID=${6}
 
 EOF
+
+# [END anthos_scripts_create_vars]
