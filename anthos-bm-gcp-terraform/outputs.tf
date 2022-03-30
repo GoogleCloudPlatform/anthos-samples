0e42ecb1c5e0e67bfbc1dc958de91306999c1166
shabirmean <shabirmean@google.com>
2022-03-30 19:26:52 -0400

    doc: fix the output var defs

--- anthos-bm-gcp-terraform/outputs.tf
+++ anthos-bm-gcp-terraform/outputs.tf
@@ -55,10 +55,9 @@ output "controlplane_ip" {
   description = <<EOF
     You may access the control plane nodes of the Anthos on bare metal cluster
     by accessing this IP address. You need to copy the kubeconfig file for the
-    cluster from the admin workstation [${local.admin_vm_hostnames}] to access
-    using the kubectl CLI.
+    cluster from the admin workstation to access using the kubectl CLI.
   EOF
-  value       = var.mode == "manuallb" ? module.configure_controlplane_lb[0].public_ip : null
+  value       = var.mode == "manuallb" ? local.contorlplane_lb_ip : null
 }
 
 output "ingress_ip" {
@@ -66,5 +65,5 @@ output "ingress_ip" {
     You may access the application deployed in the Anthos on bare metal cluster
     by accessing this IP address
   EOF
-  value       = var.mode == "manuallb" ? module.configure_ingress_lb[0].public_ip : null
+  value       = var.mode == "manuallb" ? local.ingress_lb_ip : null
 }
