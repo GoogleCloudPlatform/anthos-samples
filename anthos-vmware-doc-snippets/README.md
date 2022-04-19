532001c8ef449d155235442fe3e24f308b283319
shabirmean <shabirmean@google.com>
2022-04-19 11:07:19 -0400

    process: a4vmware permission script

---
+++ anthos-vmware-doc-snippets/README.md
@@ -0,0 +1,4 @@
+# Anthos clusters on VMware documentation snippets
+
+This directory contains reference material used by the public documentation for
+_Anthos clusters on VMware_ in [cloud.google.com](https://cloud.google.com/anthos/clusters/docs/on-prem).
---
+++ anthos-vmware-doc-snippets/assign_permissions.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# this script creates a new vCenter role "anthos" with all the required
+# permissions for Anthos clusters on VMware users
+
+if [[ -z "${GOVC_USERNAME}" ]]; then
+  echo "[x] Environment variable GOVC_USERNAME is not set."
+  exit 1
+fi
+
+if [[ -z "${GOVC_PASSWORD}" ]]; then
+  echo "[x] Environment variable GOVC_PASSWORD is not set."
+  exit 1
+fi
+
+if [[ -z "${CLUSTER_USER_ACCOUNT}" ]]; then
+  echo "[x] Environment variable CLUSTER_USER_ACCOUNT is not set."
+  exit 1
+fi
+
+if ! command -v govc &> /dev/null; then
+    echo "[x] govc CLI could not be found."
+    exit 1
+fi
+
+# [START anthosvmware_anthos_vmware_doc_snippets_assign_permissions]
+cat <<END | xargs govc role.create anthos 
+Datastore.AllocateSpace
+Datastore.Browse
+Datastore.Config
+Datastore.DeleteFile
+Datastore.FileManagement
+Datastore.UpdateVirtualMachineFiles
+Datastore.UpdateVirtualMachineMetadata
+Folder.Create
+Folder.Delete
+Folder.Move
+Folder.Rename
+Host.Inventory.EditCluster
+InventoryService.Tagging.CreateTag
+Network.Assign
+Resource.ApplyRecommendation
+Resource.AssignVMToPool
+Resource.ColdMigrate
+Resource.HotMigrate
+Resource.QueryVMotion
+Sessions.ValidateSession
+StorageViews.View
+System.Anonymous
+System.Read
+System.View
+Task.Create
+Task.Update
+VApp.ApplicationConfig
+VApp.Import
+VApp.InstanceConfig
+VirtualMachine.Config.AddExistingDisk
+VirtualMachine.Config.AddNewDisk
+VirtualMachine.Config.AddRemoveDevice
+VirtualMachine.Config.AdvancedConfig
+VirtualMachine.Config.Annotation
+VirtualMachine.Config.CPUCount
+VirtualMachine.Config.ChangeTracking
+VirtualMachine.Config.DiskExtend
+VirtualMachine.Config.DiskLease
+VirtualMachine.Config.EditDevice
+VirtualMachine.Config.HostUSBDevice
+VirtualMachine.Config.ManagedBy
+VirtualMachine.Config.Memory
+VirtualMachine.Config.MksControl
+VirtualMachine.Config.QueryFTCompatibility
+VirtualMachine.Config.QueryUnownedFiles
+VirtualMachine.Config.RawDevice
+VirtualMachine.Config.ReloadFromPath
+VirtualMachine.Config.RemoveDisk
+VirtualMachine.Config.Rename
+VirtualMachine.Config.ResetGuestInfo
+VirtualMachine.Config.Resource
+VirtualMachine.Config.Settings
+VirtualMachine.Config.SwapPlacement
+VirtualMachine.Config.ToggleForkParent
+VirtualMachine.Config.UpgradeVirtualHardware
+VirtualMachine.GuestOperations.Execute
+VirtualMachine.GuestOperations.Modify
+VirtualMachine.GuestOperations.ModifyAliases
+VirtualMachine.GuestOperations.Query
+VirtualMachine.GuestOperations.QueryAliases
+VirtualMachine.Hbr.ConfigureReplication
+VirtualMachine.Hbr.MonitorReplication
+VirtualMachine.Hbr.ReplicaManagement
+VirtualMachine.Interact.AnswerQuestion
+VirtualMachine.Interact.Backup
+VirtualMachine.Interact.ConsoleInteract
+VirtualMachine.Interact.CreateScreenshot
+VirtualMachine.Interact.CreateSecondary
+VirtualMachine.Interact.DefragmentAllDisks
+VirtualMachine.Interact.DeviceConnection
+VirtualMachine.Interact.DisableSecondary
+VirtualMachine.Interact.DnD
+VirtualMachine.Interact.EnableSecondary
+VirtualMachine.Interact.GuestControl
+VirtualMachine.Interact.MakePrimary
+VirtualMachine.Interact.Pause
+VirtualMachine.Interact.PowerOff
+VirtualMachine.Interact.PowerOn
+VirtualMachine.Interact.PutUsbScanCodes
+VirtualMachine.Interact.Record
+VirtualMachine.Interact.Replay
+VirtualMachine.Interact.Reset
+VirtualMachine.Interact.SESparseMaintenance
+VirtualMachine.Interact.SetCDMedia
+VirtualMachine.Interact.SetFloppyMedia
+VirtualMachine.Interact.Suspend
+VirtualMachine.Interact.TerminateFaultTolerantVM
+VirtualMachine.Interact.ToolsInstall
+VirtualMachine.Interact.TurnOffFaultTolerance
+VirtualMachine.Inventory.Create
+VirtualMachine.Inventory.CreateFromExisting
+VirtualMachine.Inventory.Delete
+VirtualMachine.Inventory.Move
+VirtualMachine.Inventory.Register
+VirtualMachine.Inventory.Unregister
+VirtualMachine.Namespace.Event
+VirtualMachine.Namespace.EventNotify
+VirtualMachine.Namespace.Management
+VirtualMachine.Namespace.ModifyContent
+VirtualMachine.Namespace.Query
+VirtualMachine.Namespace.ReadContent
+VirtualMachine.Provisioning.Clone
+VirtualMachine.Provisioning.CloneTemplate
+VirtualMachine.Provisioning.CreateTemplateFromVM
+VirtualMachine.Provisioning.Customize
+VirtualMachine.Provisioning.DeployTemplate
+VirtualMachine.Provisioning.DiskRandomAccess
+VirtualMachine.Provisioning.DiskRandomRead
+VirtualMachine.Provisioning.FileRandomAccess
+VirtualMachine.Provisioning.GetVmFiles
+VirtualMachine.Provisioning.MarkAsTemplate
+VirtualMachine.Provisioning.MarkAsVM
+VirtualMachine.Provisioning.ModifyCustSpecs
+VirtualMachine.Provisioning.PromoteDisks
+VirtualMachine.Provisioning.PutVmFiles
+VirtualMachine.Provisioning.ReadCustSpecs
+VirtualMachine.State.CreateSnapshot
+VirtualMachine.State.RemoveSnapshot
+VirtualMachine.State.RenameSnapshot
+VirtualMachine.State.RevertToSnapshot
+END
+# [END anthosvmware_anthos_vmware_doc_snippets_assign_permissions]
+
+govc permissions.set -principal "$CLUSTER_USER_ACCOUNT@vsphere.local" -role anthos -propagate=true
