7f0a884c2df6527a41b810c195d96e531dfb202a
Huy Pham <11185878+hdp617@users.noreply.github.com>
2022-04-28 19:46:30 +0000

    feat: Allow setting multiple admin users.

--- anthos-multi-cloud/AWS/variables.tf
+++ anthos-multi-cloud/AWS/variables.tf
@@ -26,9 +26,9 @@ variable "name_prefix" {
 
 # This step sets up the default RBAC policy in your cluster for a Google
 # user so you can login after cluster creation
-variable "admin_user" {
+variable "admin_users" {
   description = "User to get default Admin RBAC"
-  type        = string
+  type        = list(string)
 }
 
 variable "cluster_version" {
