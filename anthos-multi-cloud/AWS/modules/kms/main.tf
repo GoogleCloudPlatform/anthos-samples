5171d810a5556041dadbcbdb4193a36944d723bf
Brian Kaufman <brian.kaufman@gmail.com>
2022-05-01 12:56:13 -0400

    remove whitespace

--- anthos-multi-cloud/AWS/modules/kms/main.tf
+++ anthos-multi-cloud/AWS/modules/kms/main.tf
@@ -36,4 +36,3 @@ resource "aws_kms_alias" "database_encryption_kms_key_alias" {
   target_key_id = aws_kms_key.database_encryption_kms_key.arn
   name          = "alias/anthos-${var.anthos_prefix}-database-encryption-key"
 }
-   
