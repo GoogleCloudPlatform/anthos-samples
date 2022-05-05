8c7f5755d2b5519326bd41e0df68aca927dec724
shabirmean <shabirmean@google.com>
2022-05-05 18:45:38 -0400

    lint: shellcheck ignore

--- anthos-bm-edge-deployment/scripts/status.sh
+++ anthos-bm-edge-deployment/scripts/status.sh
@@ -13,13 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# shellcheck disable=SC1091
-
 # [START anthosbaremetal_scripts_status]
 
 # Use the path to this script to determine the path to gce-helper.vars
 PREFIX_DIR=$(dirname -- "$0")
-# shellcheck source=cloud/gce-helper.vars
+# shellcheck source=/dev/null
 source "${PREFIX_DIR}/cloud/gce-helper.vars"
 
 display_gce_vms_ips
