069ced12f71e4d095fe78882b722c2bcdec1fc9e
shabirmean <shabirmean@google.com>
2022-05-05 18:19:59 -0400

    lint: skip source follow

--- anthos-bm-edge-deployment/scripts/status.sh
+++ anthos-bm-edge-deployment/scripts/status.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# shellcheck disable=SC1091
+# shellcheck disable=SC1091,SC1090
 
 # [START anthosbaremetal_scripts_status]
 
