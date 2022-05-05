416aaf4f02fab84b862b391ff7928fb5267fe042
shabirmean <shabirmean@google.com>
2022-05-05 18:27:09 -0400

    lint: skip shellcheck error

--- anthos-bm-edge-deployment/scripts/status.sh
+++ anthos-bm-edge-deployment/scripts/status.sh
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# shellcheck disable=SC1091,SC1090
+# shellcheck disable=SC1091
+# shellcheck source=/dev/null
 
 # [START anthosbaremetal_scripts_status]
 
