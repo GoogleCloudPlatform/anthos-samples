cf3ea6a69f75e2624bf6b59a7f220f95c02f3e09
shabirmean <shabirmean@google.com>
2022-05-05 20:12:06 -0400

    lint: update the shellcheck

--- anthos-bm-edge-deployment/scripts/status.sh
+++ anthos-bm-edge-deployment/scripts/status.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# shellcheck disable=SC1091,SC1090
 # Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +17,7 @@
 
 # Use the path to this script to determine the path to gce-helper.vars
 PREFIX_DIR=$(dirname -- "$0")
+# shellcheck source=./cloud/gce-helper.vars
 source "${PREFIX_DIR}/cloud/gce-helper.vars"
 
 display_gce_vms_ips
