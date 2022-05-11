5dc835f31b10fad786af5caa94e094a55d542867
shabirmean <shabirmean@google.com>
2022-05-11 11:21:14 -0400

    doc: fix the shell script error

--- anthos-bm-edge-deployment/scripts/cloud/easy-install.sh
+++ anthos-bm-edge-deployment/scripts/cloud/easy-install.sh
@@ -38,7 +38,7 @@ echo "💡 -------------------------------------------------------------------"
 echo "💡 -------------------------------------------------------------------"
 echo "💡 Updating /etc/hosts with the IP addresses of the GCE instances..."
 echo "💡 -------------------------------------------------------------------"
-"${ROOT_DIR}"/scripts/status.sh | tail -$(("${MACHINE_COUNT}"+1)) > temp.log
+"${ROOT_DIR}"/scripts/status.sh | tail -"$((${MACHINE_COUNT}+1))" > temp.log
 sudo sh -c 'cat temp.log >> /etc/hosts'
 rm -rf temp.log
 
