0f97a67dd601af279ee9b91ff6986de2f738e2a4
Shabir Mohamed Abdul Samadh <7249208+Shabirmean@users.noreply.github.com>
2022-07-25 11:21:15 -0400

    revert: changes to terraform version

--- anthos-bm-gcp-terraform/versions.tf
+++ anthos-bm-gcp-terraform/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = ">= v0.11.3, < 1.2" # this line should not change during a release
+  required_version = ">= v0.15.5, < 1.2" # this line should not change during a release
   required_providers {
     google = {
       source  = "hashicorp/google"
