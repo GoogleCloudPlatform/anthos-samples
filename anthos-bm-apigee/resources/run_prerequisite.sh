5e9edadcdffe35213d036b6a5a17036ab4c4c128
shabirmean <shabirmean@google.com>
2022-09-18 22:55:29 -0400

    cleanup: addlicense and fix gitignore

--- anthos-bm-apigee/resources/run_prerequisite.sh
+++ anthos-bm-apigee/resources/run_prerequisite.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 project=$(gcloud config get-value project)
 export  project
@@ -60,7 +73,6 @@ create_owner_service_account() {
 	gcloud iam service-accounts keys create anthos-bm-owner.json --iam-account=baremetal-owner@"${project}".iam.gserviceaccount.com
 	gcloud projects add-iam-policy-binding "${project}" --member=serviceAccount:baremetal-owner@"${project}".iam.gserviceaccount.com --role=roles/owner
 	gcloud auth activate-service-account --key-file anthos-bm-owner.json
-
 }
 
 enable_services
