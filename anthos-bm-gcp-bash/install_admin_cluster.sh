639ac4ba25b04b599ce620cffaf4e8b8ca23b741
shedyb <36378295+shedyb@users.noreply.github.com>
2024-07-24 17:25:05 -0400

    fix: Add more APIs and role binding to the prerequisite #678 (#679)
    * Adding more APIs and role binding to the prerequisite #678
    
    * Adding more rolebinding to existing PR
    
    ---------
    
    Co-authored-by: Nim Jayawardena <i.am.nim.jay@gmail.com>

--- anthos-bm-gcp-bash/install_admin_cluster.sh
+++ anthos-bm-gcp-bash/install_admin_cluster.sh
@@ -108,6 +108,7 @@ gcloud services enable \
     cloudresourcemanager.googleapis.com \
     connectgateway.googleapis.com \
     container.googleapis.com \
+    compute.googleapis.com \
     gkeconnect.googleapis.com \
     gkehub.googleapis.com \
     gkeonprem.googleapis.com \
@@ -115,6 +116,8 @@ gcloud services enable \
     stackdriver.googleapis.com \
     monitoring.googleapis.com \
     logging.googleapis.com \
+    kubernetesmetadata.googleapis.com  \
+    iam.googleapis.com \
     opsconfigmonitoring.googleapis.com
 # [END anthos_bm_gcp_bash_admin_enable_api]
 printf "✅ Successfully enabled GCP Service APIs.\n\n"
@@ -156,6 +159,22 @@ gcloud projects add-iam-policy-binding "$PROJECT_ID" \
   --member="serviceAccount:baremetal-gcr@$PROJECT_ID.iam.gserviceaccount.com" \
   --role="roles/opsconfigmonitoring.resourceMetadata.writer" \
   --no-user-output-enabled
+
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    --member="serviceAccount:baremetal-gcr@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/kubernetesmetadata.publisher" \
+    --no-user-output-enabled 
+
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+  --member="serviceAccount:baremetal-gcr@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/monitoring.viewer" \
+  --no-user-output-enabled
+
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+  --member="serviceAccount:baremetal-gcr@$PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/serviceusage.serviceUsageViewer" \
+  --no-user-output-enabled
+
 # [END anthos_bm_gcp_bash_admin_add_iam_role]
 printf "✅ Successfully added the requires IAM roles to the Service Account.\n\n"
 
