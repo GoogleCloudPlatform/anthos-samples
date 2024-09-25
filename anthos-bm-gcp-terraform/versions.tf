391ea4465b5d349ba5bef18ed5cbda4c87ec7d40
Andrew Peabody <andrewpeabody@google.com>
2024-09-25 16:05:29 -0700

    Update versions.tf

--- anthos-bm-gcp-terraform/versions.tf
+++ anthos-bm-gcp-terraform/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = ">= v0.15.4, < 1.10" # this line should not change during a release
+  required_version = ">= v0.15.3, < 1.10" # this line should not change during a release
   required_providers {
     google = {
       source  = "hashicorp/google"
