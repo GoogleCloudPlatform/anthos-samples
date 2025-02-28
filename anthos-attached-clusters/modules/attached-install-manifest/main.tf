a623b7514f7008fc654d6b3ac09b41b34d301df7
Andrew Peabody <andrewpeabody@google.com>
2025-02-28 01:21:45 +0000

    fix: update attached modules

--- anthos-attached-clusters/modules/attached-install-manifest/main.tf
+++ anthos-attached-clusters/modules/attached-install-manifest/main.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2024-2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ resource "local_file" "bootstrap_helm_chart" {
   content  = <<-EOT
     apiVersion: v2
     name: ${local.helm_chart_name}
-    version: 0.0.0
+    version: ${var.platform_version}
     type: application
     EOT
 }
